eda268c0fc3886d59976ff35886bfb8379d67229
BishalMukherjee <bishal.mukherjee@kreeti.com>
2024-06-28 11:30:26 +0530

    Addition of multiple numbers from the string input

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,13 @@
 class Calculator
+  def add(input_string)
+    numbers = input_string.split(",")
+
+    result = 0
+
+    numbers.each do |n|
+      result = result + n.to_i
+    end
+
+    return result
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,4 +2,15 @@ require 'rspec'
 require_relative '../calculator'
 
 RSpec.describe Calculator do
+  let(:calculator) { Calculator.new }
+
+  describe "#add" do
+    it "empty string should return zero" do
+      expect(calculator.add("")).to eq(0)
+    end
+
+    it "add two numbers in a string separated by comma" do
+      expect(calculator.add("1,2")).to eq(3)
+    end
+  end
 end
